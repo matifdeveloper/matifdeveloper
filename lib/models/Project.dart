eca6a6408bb6ff2da0004628a742dcfc4c38249b
matifdeveloper <atifnoori.official@gmail.com>
2023-08-16 15:18:35 +0500

    matifdeveloper

--- lib/models/Project.dart
+++ lib/models/Project.dart
@@ -53,6 +53,16 @@ With Nest - Your Al Parenting Companion, we strive to provide expectant mothers 
         'https://play.google.com/store/apps/details?id=com.metasense.nest',
   ),
   Project(
+    title: "Play Now - Pickup Soccer",
+    description: 'Play NOW is a soccer app for on demand pickup soccer in the Boston area. We are Boston’s #1 group for pickup soccer.',
+    appstore: 'https://apps.apple.com/pk/app/play-now-pickup-soccer/id6444154431',
+  ),
+  Project(
+    title: "Masters Barbershop",
+    description: 'Master Barber Shop is here to revolutionize your grooming routine and connect you with the finest barbers in town. With our user-friendly app, you can effortlessly browse through a curated selection of skilled barbers, their exceptional services, transparent prices, and so much more. Get ready to elevate your grooming game like never before!.',
+    appstore: 'https://apps.apple.com/us/app/masters-barbershop/id1670807810',
+  ),
+  Project(
     title: "HD Wallpapers 2023",
     description: 'HD Wallpapers is a free wallpaper app that contains high-definition backgrounds for everyone.',
     playstore: 'https://play.google.com/store/apps/details?id=com.noorisofttech.hd_wallpapers',
@@ -61,6 +71,22 @@ With Nest - Your Al Parenting Companion, we strive to provide expectant mothers 
     title: "Taj Personal Care",
     description: 'Taj Personal Care offers the best AL Taj AL Malaki therapeutic Spa and Treatment in Dubai Taj Spa professional therapists are from AL Taj AL Malaki therapeutic Spa is a complete combination of Asian Treatment therapists’ expertise, a blend of world aroma, mythological roots, and fascinating ambiance.',
     playstore: 'https://play.google.com/store/apps/details?id=com.metasense.taj_spa',
+  ),
+  Project(
+    title: "Text Repeater",
+    description: 'I have create Text Repeater app, an ingenious mobile application that simplifies text duplication. By using this application you can send the same message at multiple times. Text Repeater app is a best way to repeat the text as per your choice. Repeat text and Emoji’s as many times you want.',
+    playstore: 'https://play.google.com/store/apps/details?id=com.noorisofttech.text_repeater',
+  ),
+  Project(
+    title: "All Sim Packages",
+    description: 'All Sim Packages 2023 provide you all information about all mobile network packages in Pakistan. You can check easily all available packages.',
+    playstore: 'https://play.google.com/store/apps/details?id=com.noorisofttech.allsimpackages',
+  ),
+  Project(
+    title: "My Credit Score",
+    description: 'My Credit Score South Africa\'s vision is to assist everyone, regardless of circumstance, in achieving greater financial well-being. We began this journey by providing everyone with free access to their credit score and report for the rest of their lives. Join over 10 million users worldwide who are using My Credit Score to take control of their finances today.',
+    web: 'https://app.credit-score.app/#/minified:w8',
+    playstore: 'https://play.google.com/store/apps/details?id=com.metasense.credit_score&hl=en&gl=US',
   ),
   Project(
     title: "Mana Care",
